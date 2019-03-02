4bc1da30e3a04e67f3bae13c59e3624a10c3b7d5
umangg21 <umangg21@gmail.com>
2019-03-02 22:49:40 +0530

    Updates

---
+++ precache-manifest.7b5fdc9b843e438cd2a9e5c2a3c3a24d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b7fa050b768968df1c",
+    "url": "/rss-feed/static/js/runtime~main.f3b7fa05.js"
+  },
+  {
+    "revision": "04f38feff64c0bd2e9d0",
+    "url": "/rss-feed/static/js/main.04f38fef.chunk.js"
+  },
+  {
+    "revision": "ecd1336c9f74a1b1755c",
+    "url": "/rss-feed/static/js/2.ecd1336c.chunk.js"
+  },
+  {
+    "revision": "2f017d48120a0c0e2301409a82358034",
+    "url": "/rss-feed/index.html"
+  }
+];
